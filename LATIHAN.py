cffe09ab16e801dda37c388064f5e9ca0b7c7ad8
izzdevjournay <izzcoding0@gmail.com>
2024-10-22 21:13:59 +0700

    Add files via upload

---
+++ LATIHAN.py
@@ -0,0 +1,19 @@
+print("====== ORIGINAL ======")
+
+a = 7
+b = a ^ 2
+print('Nilai dari a ^ 2 adalah ',a)
+
+c = True
+c = c | False
+print('Nilai dari c | False adalah ',c)
+
+print("====== Operator Asigment ======")
+
+x = 10
+y = x ^ 2
+print('Nilai dari x ^ 2 adalah ',x)
+
+y = True
+y = y | True
+print('Nilai dari y | True adalah ',y)
